2ee5d627cde59a3f677800adb7c4f8e862d458dc
Alfah <nepta283441@gmail.com>
2024-10-21 18:05:05 +0700

    Tugas_DOM

---
+++ Tugas_DOM/script.js
@@ -0,0 +1,8 @@
+document.getElementById('updateButton').addEventListener('click', function() {
+  // Update profile title and description
+  document.getElementById('profileTitle').innerText = "Tentang Nepta V.02";
+  document.getElementById('profileDescription').innerText = "Saya suka hikking healing dan mendaki gunung";
+
+  // Update about section text
+  document.getElementById('aboutText').innerText = "Karakter anime yang aku suka adalah Rin Shima";
+});
